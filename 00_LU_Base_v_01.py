e48ef5cf09b278b88c820c8c0734cad4bc5164a1
bigmantings69 <78823126+bigmantings69@users.noreply.github.com>
2021-02-18 18:28:37 +1300

    lucky unicorn base

---
+++ 00_LU_Base_v_01.py
@@ -0,0 +1,35 @@
+
+# Function go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("please enter yes or no")
+
+
+def instructions():
+    print("**** How to Play ****")
+    print()
+    print("The rules of the game go here")
+    print()
+    return""
+
+# Main Routine goes here...
+
+
+played_before = yes_no("Have you played the ""game before? ")
+
+if played_before == "no":
+    instructions()
+
+print("Programs Continue")
